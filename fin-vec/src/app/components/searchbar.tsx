2f14cc7442e61e179d9171da1de3eb3692477e41
dkeefe3 <dkeefe3@gatech.edu>
2024-04-26 15:34:18 -0400

    Removed comments

--- fin-vec/src/app/components/searchbar.tsx
+++ fin-vec/src/app/components/searchbar.tsx
@@ -12,25 +12,11 @@ import Graph from './graph';
 export default function Search({ placeholder }: { placeholder: string }) {
     const [ticker, setTicker] = useState('');
     const [date, setDate] = useState('');
-    //const [industry, setIndustry] = useState('');
     const [k, setK] = useState('');
     const [searchResults, setSearchResults] = useState<Record<string, any[]>>({});
     const [priceOption, setPriceOption] = useState('');
 
     const priceOptions = ["Underperforming", "Overperforming"];
-    /*
-    const industries = ['Heathcare', 'Retail', 'Healthcare', 'Business', 'Software', 'Semiconductor',
-        'Farming', 'Utility', 'Power', 'Insurance', 'Chemical', 'Air', 'Electrical',
-        'Drugs', 'R.E.I.T.', 'Building', 'Electronics', 'Steel', 'Food', 'Aerospace',
-        'Bank', 'Investments', 'Beverage', 'Information', 'Auto', 'Household', 'Oil',
-        'Cable', 'Financial', 'Engineering', 'Telecom', 'Transportation',
-        'Homebuilding', 'Machinery', 'Restaurant', 'Entertainment', 'Hotel', 'Apparel',
-        'Diversified', 'Rubber', 'Oilfield', 'Recreation', 'Computers', 'Computer',
-        'Advertising', 'Trucking', 'Brokerage', 'Furn', 'Construction', 'Tobacco',
-        'Shoe', 'Packaging', 'Office', 'Environmental', 'Hospitals', 'Real', 'Telecom',
-        'Education', 'Banks', 'Precious', 'Reinsurance', 'Broadcasting', 'Publishing',
-        'Metals'];
-    */
 
     const handleTickerChange = (e: ChangeEvent<HTMLInputElement>) => {
         setTicker(e.target.value);
@@ -39,12 +25,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
         setDate(e.target.value);
     };
-
-    /*
-    const handleIndustryChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setIndustry(e.target.value);
-    };
-    */
 
     const handleKChange = (e: ChangeEvent<HTMLInputElement>) => {
         setK(e.target.value);
@@ -57,7 +37,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     async function handleSearch() {
         console.log("Ticker: " + ticker);
         console.log("Date: " + date);
-        //console.log("Industry: " + industry);
 
         if (ticker === '') {
             return;
@@ -65,7 +44,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
         console.log("Searching for " + ticker);
         try {
             let dateCopy = new Date(date);
-            // get next tuesday of date if date is before 2022
             if (dateCopy.getFullYear() < 2022) {
                 dateCopy.setDate(dateCopy.getDate() + (1 + 7 - dateCopy.getDay()) % 7);
             }
@@ -106,16 +84,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
                             ))}
                         </select>
                     </div>
-
-                    {/* <div>
-                        <label htmlFor="industry" className="block text-sm font-medium text-gray-700">Industry [Optional]:</label>
-                        <select id="industry" name="industry" value={industry} onChange={handleIndustryChange} className="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                            <option value="">Select an industry</option>
-                            {industries.map((industry, index) => (
-                                <option key={index} value={industry}>{industry}</option>
-                            ))}
-                        </select>
-                    </div> */}
 
                     <button type="submit" className="mt-4 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Run Query</button>
                 </form>
